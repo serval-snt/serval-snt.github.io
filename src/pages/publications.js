1bfcd3541ec372521c2a928356b510136f652bc2
Guillaume Haben <pyxouze@gmail.com>
2020-04-29 11:02:04 +0200

    Reorder publications
    Add Kui and Wei University ID to the list

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -16,93 +16,93 @@ const IndexPage = ({ data }) => {
       <SEO title="Publications" />
       <Content>
         <h1>Publications</h1>
-        <h3>Refereed Journal Articles</h3>
-        <iframe src="https://orbilu.uni.lu/widget?query=%28
-        %28author%3AYves LE TRAON%29+OR+
-        %28author%3AAlexandre BARTEL%29+OR+
-        %28author%3AAndrey BOYSTOV%29+OR+
-        %28author%3ARenzo DEGIOVANNI%29+OR+
-        %28author%3ASankalp GHATPANDE%29+OR+
-        %28author%3AMédéric HURIER%29+OR+
-        %28author%3ALudovic MOULINE%29+OR+
-        %28author%3AJérémy ROBERT%29+OR+
-        %28author%3AImen SAYAR%29+OR+
-        %28author%3AThierry TITCHEU CHEKAM%29+OR+
-        %28author%3AKui LIU%29+OR+
-        %28author%3AMaxime CORDY%29+OR+
-        %28author%3AMike PAPADAKIS%29+OR+
-        %28author%3ANikolaos ANTONIADIS%29+OR+
-        %28author%3APaul-Lou BENEDICK%29+OR+
-        %28author%3AAayush GARG%29+OR+
-        %28author%3ASalah GHAMIZI%29+OR+
-        %28author%3AMartin GUBRI%29+OR+
-        %28author%3AGuillaume HABEN%29+OR+
-        %28author%3AAhmed KHANFIR%29+OR+
-        %28author%3ANiklas KOLBE%29+OR+
-        %28author%3AWei MA%29+OR+
-        %28author%3ARenaud RWEMALIKA%29+OR+
-        %28author%3AMilos OJDANIC%29+OR+
-        %28author%3AXavier SCHMITT%29%29
-        +AND+%28%28type%3A%23A00%29%29&amp;chars=100&amp;etal=3&amp;language=en&amp;data=pr&amp;format=apa&amp;css=%2Ffiles%2Fcss%2Fwl.css&amp;sort_by0=1&amp;order0=DESC&amp;sort_by1=3&amp;order1=ASC&amp;sort_by2=2&amp;order2=ASC" marginwidth="0" marginheight="0" frameborder="0" height="450" scrolling="auto" width="860"></iframe>
 
         <h3>Refereed Conference Articles</h3>
 
-        <iframe src="https://orbilu.uni.lu/widget?query=%28
-        %28author%3AYves LE TRAON%29+OR+
-        %28author%3AAlexandre BARTEL%29+OR+
-        %28author%3AAndrey BOYSTOV%29+OR+
-        %28author%3ARenzo DEGIOVANNI%29+OR+
-        %28author%3ASankalp GHATPANDE%29+OR+
-        %28author%3AMédéric HURIER%29+OR+
-        %28author%3ALudovic MOULINE%29+OR+
-        %28author%3AJérémy ROBERT%29+OR+
-        %28author%3AImen SAYAR%29+OR+
-        %28author%3AThierry TITCHEU CHEKAM%29+OR+
-        %28author%3AKui LIU%29+OR+
-        %28author%3AMaxime CORDY%29+OR+
-        %28author%3AMike PAPADAKIS%29+OR+
-        %28author%3ANikolaos ANTONIADIS%29+OR+
-        %28author%3APaul-Lou BENEDICK%29+OR+
-        %28author%3AAayush GARG%29+OR+
-        %28author%3ASalah GHAMIZI%29+OR+
-        %28author%3AMartin GUBRI%29+OR+
-        %28author%3AGuillaume HABEN%29+OR+
-        %28author%3AAhmed KHANFIR%29+OR+
-        %28author%3ANiklas KOLBE%29+OR+
-        %28author%3AWei MA%29+OR+
-        %28author%3ARenaud RWEMALIKA%29+OR+
-        %28author%3AMilos OJDANIC%29+OR+
-        %28author%3AXavier SCHMITT%29%29
+        <iframe title="conferences" src="https://orbilu.uni.lu/widget?query=%28
+        %28author%3AYves+author%3ALE+author%3ATRAON%29+OR+
+        %28author%3AAlexandre+author%3ABARTEL%29+OR+
+        %28author%3AAndrey+author%3ABOYSTOV%29+OR+
+        %28author%3ARenzo+author%3ADEGIOVANNI%29+OR+
+        %28author%3ASankalp+author%3AGHATPANDE%29+OR+
+        %28author%3AMédéric+author%3AHURIER%29+OR+
+        %28author%3ALudovic+author%3AMOULINE%29+OR+
+        %28author%3AJérémy+author%3AROBERT%29+OR+
+        %28author%3AImen+author%3ASAYAR%29+OR+
+        %28author%3AThierry+author%3ATITCHEU+author%3ACHEKAM%29+OR+
+        %28author%3AKui+author%3ALIU%29+OR+
+        %28author%3AMaxime+author%3ACORDY%29+OR+
+        %28author%3AMike+author%3APAPADAKIS%29+OR+
+        %28author%3ANikolaos+author%3AANTONIADIS%29+OR+
+        %28author%3APaul-Lou+author%3ABENEDICK%29+OR+
+        %28author%3AAayush+author%3AGARG%29+OR+
+        %28author%3ASalah+author%3AGHAMIZI%29+OR+
+        %28author%3AMartin+author%3AGUBRI%29+OR+
+        %28author%3AGuillaume+author%3AHABEN%29+OR+
+        %28author%3AAhmed+author%3AKHANFIR%29+OR+
+        %28author%3ANiklas+author%3AKOLBE%29+OR+
+        %28author%3AWei+author%3AMA%29+OR+
+        %28author%3ARenaud+author%3ARWEMALIKA%29+OR+
+        %28author%3AMilos+author%3AOJDANIC%29+OR+
+        %28author%3AXavier+author%3ASCHMITT%29%29
         +AND+%28%28type%3A%23D00%29%29&amp;chars=100&amp;etal=3&amp;language=en&amp;data=pr&amp;format=apa&amp;css=%2Ffiles%2Fcss%2Fwl.css&amp;sort_by0=1&amp;order0=DESC&amp;sort_by1=3&amp;order1=ASC&amp;sort_by2=2&amp;order2=ASC" marginwidth="0" marginheight="0" frameborder="0" height="450" scrolling="auto" width="860"></iframe>
+
+        <h3>Refereed Journal Articles</h3>
+        
+        <iframe title="journals" src="https://orbilu.uni.lu/widget?query=%28
+        %28author%3AYves+author%3ALE+author%3ATRAON%29+OR+
+        %28author%3AAlexandre+author%3ABARTEL%29+OR+
+        %28author%3AAndrey+author%3ABOYSTOV%29+OR+
+        %28author%3ARenzo+author%3ADEGIOVANNI%29+OR+
+        %28author%3ASankalp+author%3AGHATPANDE%29+OR+
+        %28author%3AMédéric+author%3AHURIER%29+OR+
+        %28author%3ALudovic+author%3AMOULINE%29+OR+
+        %28author%3AJérémy+author%3AROBERT%29+OR+
+        %28author%3AImen+author%3ASAYAR%29+OR+
+        %28author%3AThierry+author%3ATITCHEU+author%3ACHEKAM%29+OR+
+        %28author%3AMaxime+author%3ACORDY%29+OR+
+        %28author%3AMike+author%3APAPADAKIS%29+OR+
+        %28author%3ANikolaos+author%3AANTONIADIS%29+OR+
+        %28author%3APaul-Lou+author%3ABENEDICK%29+OR+
+        %28author%3AAayush+author%3AGARG%29+OR+
+        %28author%3ASalah+author%3AGHAMIZI%29+OR+
+        %28author%3AMartin+author%3AGUBRI%29+OR+
+        %28author%3AGuillaume+author%3AHABEN%29+OR+
+        %28author%3AAhmed+author%3AKHANFIR%29+OR+
+        %28author%3ANiklas+author%3AKOLBE%29+OR+
+        %28author%3ARenaud+author%3ARWEMALIKA%29+OR+
+        %28author%3AMilos+author%3AOJDANIC%29+OR+
+        %28author%3AXavier+author%3ASCHMITT%29%29
+        +AND+%28%28type%3A%23A00%29%29&amp;chars=100&amp;etal=3&amp;language=en&amp;data=pr&amp;format=apa&amp;css=%2Ffiles%2Fcss%2Fwl.css&amp;sort_by0=1&amp;order0=DESC&amp;sort_by1=3&amp;order1=ASC&amp;sort_by2=2&amp;order2=ASC" marginwidth="0" marginheight="0" frameborder="0" height="450" scrolling="auto" width="860"></iframe>
 
         <h3>Books and Book Chapters</h3>
 
-        <iframe src="https://orbilu.uni.lu/widget?query=%28
-        %28author%3AYves LE TRAON%29+OR+
-        %28author%3AAlexandre BARTEL%29+OR+
-        %28author%3AAndrey BOYSTOV%29+OR+
-        %28author%3ARenzo DEGIOVANNI%29+OR+
-        %28author%3ASankalp GHATPANDE%29+OR+
-        %28author%3AMédéric HURIER%29+OR+
-        %28author%3ALudovic MOULINE%29+OR+
-        %28author%3AJérémy ROBERT%29+OR+
-        %28author%3AImen SAYAR%29+OR+
-        %28author%3AThierry TITCHEU CHEKAM%29+OR+
-        %28author%3AKui LIU%29+OR+
-        %28author%3AMaxime CORDY%29+OR+
-        %28author%3AMike PAPADAKIS%29+OR+
-        %28author%3ANikolaos ANTONIADIS%29+OR+
-        %28author%3APaul-Lou BENEDICK%29+OR+
-        %28author%3AAayush GARG%29+OR+
-        %28author%3ASalah GHAMIZI%29+OR+
-        %28author%3AMartin GUBRI%29+OR+
-        %28author%3AGuillaume HABEN%29+OR+
-        %28author%3AAhmed KHANFIR%29+OR+
-        %28author%3ANiklas KOLBE%29+OR+
-        %28author%3AWei MA%29+OR+
-        %28author%3ARenaud RWEMALIKA%29+OR+
-        %28author%3AMilos OJDANIC%29+OR+
-        %28author%3AXavier SCHMITT%29%29
+        <iframe title="books" src="https://orbilu.uni.lu/widget?query=%28
+        %28author%3AYves+author%3ALE+author%3ATRAON%29+OR+
+        %28author%3AAlexandre+author%3ABARTEL%29+OR+
+        %28author%3AAndrey+author%3ABOYSTOV%29+OR+
+        %28author%3ARenzo+author%3ADEGIOVANNI%29+OR+
+        %28author%3ASankalp+author%3AGHATPANDE%29+OR+
+        %28author%3AMédéric+author%3AHURIER%29+OR+
+        %28author%3ALudovic+author%3AMOULINE%29+OR+
+        %28author%3AJérémy+author%3AROBERT%29+OR+
+        %28author%3AImen+author%3ASAYAR%29+OR+
+        %28author%3AThierry+author%3ATITCHEU+author%3ACHEKAM%29+OR+
+        %28author%3AKui+author%3ALIU%29+OR+
+        %28author%3AMaxime+author%3ACORDY%29+OR+
+        %28author%3AMike+author%3APAPADAKIS%29+OR+
+        %28author%3ANikolaos+author%3AANTONIADIS%29+OR+
+        %28author%3APaul-Lou+author%3ABENEDICK%29+OR+
+        %28author%3AAayush+author%3AGARG%29+OR+
+        %28author%3ASalah+author%3AGHAMIZI%29+OR+
+        %28author%3AMartin+author%3AGUBRI%29+OR+
+        %28author%3AGuillaume+author%3AHABEN%29+OR+
+        %28author%3AAhmed+author%3AKHANFIR%29+OR+
+        %28author%3ANiklas+author%3AKOLBE%29+OR+
+        %28author%3AWei+author%3AMA%29+OR+
+        %28author%3ARenaud+author%3ARWEMALIKA%29+OR+
+        %28author%3AMilos+author%3AOJDANIC%29+OR+
+        %28author%3AXavier+author%3ASCHMITT%29%29
         +AND+%28%28type%3A%23B00%29+OR+%28type%3A%23C00%29%29&amp;chars=100&amp;etal=3&amp;language=en&amp;data=pr&amp;format=apa&amp;css=%2Ffiles%2Fcss%2Fwl.css&amp;sort_by0=1&amp;order0=DESC&amp;sort_by1=3&amp;order1=ASC&amp;sort_by2=2&amp;order2=ASC" marginwidth="0" marginheight="0" frameborder="0" height="450" scrolling="auto" width="860"></iframe>
 
       </Content>
