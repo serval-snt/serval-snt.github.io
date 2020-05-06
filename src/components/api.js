94ac809914950259fe992402bd8efde3fd157e59
Guillaume Haben <pyxouze@gmail.com>
2020-05-06 14:34:31 +0200

    Fix CORS try

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let LOCAL_URL = `http://localhost:8080/`
-let REMOTE_URL = `https://cors-anywhere.herokuapp.com/https://myphd-243415.ew.r.appspot.com/`
+let REMOTE_URL = `https://myphd-243415.ew.r.appspot.com/`
 
 let baseUrl = REMOTE_URL
 export default axios.create({
