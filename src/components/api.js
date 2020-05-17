deb797accd099f725e40d3039bf3ae96ca650684
kabinja <renaudrwemalika@gmail.com>
2020-05-17 19:43:24 +0200

    change back api call to remote server

--- src/components/api.js
+++ src/components/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let LOCAL_URL = `http://localhost:8080/`
 let REMOTE_URL = `https://myphd-243415.ew.r.appspot.com/`
 
-let baseUrl = LOCAL_URL//REMOTE_URL
+let baseUrl = REMOTE_URL//LOCAL_URL
 export default axios.create({
   baseURL:baseUrl
 });
